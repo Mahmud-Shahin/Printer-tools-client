6f62072a9bc851aa190858c57f2a1200e16f5fc8
Shahin Mahmud <shahinmahmud184@gmail.com>
2024-06-15 18:13:59 +0600

    update data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 
 import './App.css'
+import Footer from './pages/Footer'
+
+
+import Home from './pages/Home'
+
 
 function App() {
   
 
   return (
     <>
+    <div className='mx-8'>
+      <Home></Home>
+      <Footer></Footer>
+    </div>
+      
     
-      <h1 className='text-4xl'>This is Tools Manufacture Company </h1>
       
      
     </>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import Header from "./Header";
+import Slider from "./Slider";
+
+
+const Home  = () => {
+
+    return (
+            <div >
+                <Header></Header>
+                <Slider></Slider>
+                
+            </div>
+ 
+) ;
+
+ }
+
+
+export default Home;
